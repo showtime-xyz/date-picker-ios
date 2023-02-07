2241f9a25c1cb01b1d3268539ab832483287713f
Tugdual de Kerviler <dekervit@gmail.com>
2023-02-07 22:43:33 +0100

    Fix for iPad for error:
    Your application has presented a UIAlertController (<UIAlertController: 0x7fa4e56dda00>) of style
    UIAlertControllerStyleActionSheet from UIViewController (<UIViewController: 0x7fa500c59390>). The modalPresentationStyle of a UIAlertController with this style is UIModalPresentationPopover. You must provide
    location information for this popover through the alert controller's popoverPresentationController. You must provide either a sourceView and sourceRect or a barButtonItem.  If this information is not known when
    you present the alert controller, you may provide it in the UIPopoverPresentationControllerDelegate method -prepareForPopoverPresentation.

--- ios/DatePickerModule.swift
+++ ios/DatePickerModule.swift
@@ -65,28 +65,29 @@ public class DatePickerModule: Module {
                 datePicker.preferredDatePickerStyle = .wheels
             }
 
-          
+
           let alert = UIAlertController(title: nil, message: nil, preferredStyle: .actionSheet)
           self.alertController = alert;
-          
+
           alert.view.addSubview(datePicker)
 
-          
+
           // make height equal to date picker and adjust for Ok and Cancel buttons
           let heightConstraint = NSLayoutConstraint(item: alert.view as Any, attribute: .height, relatedBy: .equal, toItem: datePicker, attribute: .height, multiplier: 1, constant:  2 * 80)
           alert.view.addConstraint(heightConstraint)
 
+          let widthConstraint = NSLayoutConstraint(item: alert.view as Any, attribute: .width, relatedBy: .equal, toItem: datePicker, attribute: .width, multiplier: 1, constant: 32)
+          alert.view.addConstraint(widthConstraint)
+
           // Adds some padding top to date picker
-          datePicker.bounds = datePicker.frame.insetBy(dx: 0.0, dy: -32.0);
+          datePicker.bounds = datePicker.frame
 
 
-          
           // Horizontally center date picker
           datePicker.translatesAutoresizingMaskIntoConstraints = false
           NSLayoutConstraint.activate([
             datePicker.centerXAnchor.constraint(equalTo: alert.view.centerXAnchor),
           ])
-        
 
           // Add an action to the alert controller that will trigger when the user selects a date
           let okAction = UIAlertAction(title: "OK", style: .default) { _ in
@@ -107,14 +108,19 @@ public class DatePickerModule: Module {
           alert.addAction(okAction)
           alert.addAction(cancelAction)
 
-         
 
+         
           let alertWindow = UIWindow(frame: UIScreen.main.bounds)
           alertWindow.rootViewController = UIViewController()
           alertWindow.windowLevel = UIWindow.Level.alert + 1;
           alertWindow.makeKeyAndVisible()
-          alertWindow.rootViewController?.present(alert, animated: true)
+
+          alert.popoverPresentationController?.sourceView = alertWindow
+          alert.popoverPresentationController?.sourceRect = CGRect(x: alertWindow.bounds.midX, y: alertWindow.bounds.midY, width: 0, height: 0)
+          alert.popoverPresentationController?.permittedArrowDirections = []
           
+          alertWindow.rootViewController?.present(alert, animated: true)
+
           self.alertWindow = alertWindow;
 
       }.runOnQueue(.main)
